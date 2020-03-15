291392daea6c14defd84c45d8d46d8f4be53fc85
Kaushik Chandrasekhar <kaushikc92@gmail.com>
2020-03-15 22:53:17 +0000

    UI for Input complete

--- ui/src/App.js
+++ ui/src/App.js
@@ -17,8 +17,9 @@ class App extends React.Component {
       bPathSelector: false,
       oPath: "",
       oPathSelector: false,
-      nA: "1",
-      nB: "1",
+      nA: "",
+      nB: "",
+      containerUrl: "",
       replicas: "",
       uid: "",
       fnStatus: "",
@@ -30,8 +31,8 @@ class App extends React.Component {
     this.getSpecs = this.getSpecs.bind(this);
     this.authenticateUser = this.authenticateUser.bind(this);
     this.getDriveObjects = this.getDriveObjects.bind(this);
-    //this.startBlockFn = this.startBlockFn.bind(this);
-    //this.toggleLogsPage = this.toggleLogsPage.bind(this);
+    this.startBlockFn = this.startBlockFn.bind(this);
+    this.toggleLogsPage = this.toggleLogsPage.bind(this);
   }
   getSpecs() {
     const request = axios({
@@ -98,6 +99,10 @@ class App extends React.Component {
       );
     });
   }
+  startBlockFn() {
+  }
+  toggleLogsPage() {
+  }
   render() {
     if (Object.keys(this.state.specs).length === 0) {
       this.getSpecs();
@@ -110,17 +115,85 @@ class App extends React.Component {
         <ConsoleOutput />
       );
     } else {
+      let aPath, bPath;
+      function getName(cDrivePath) {
+        if (cDrivePath === "") {
+          return ""
+        }
+        return cDrivePath.substring(cDrivePath.lastIndexOf("/") + 1);
+      }
+      aPath = getName(this.state.aPath);
+      if (aPath === "") {
+        aPath = "Choose Table A"
+      }
+      bPath = getName(this.state.bPath);
+      if (bPath === "") {
+        bPath = "Choose Table B";
+      }
+      let blockButton;
+      blockButton = (
+        <button className="btn btn-lg btn-primary blocker-btn">
+          Execute
+        </button>
+      );
+      let abortButton;
+      abortButton = (
+        <button className="btn btn-lg btn-secondary blocker-btn">
+          Abort
+        </button>
+      );
       return(
         <div className="app-container">
+          <div className="app-header">
+            Blocker
+          </div>
           <CDrivePathSelector show={this.state.aPathSelector} toggle={() => this.setState({aPathSelector : false})}
           action={path => this.setState({aPath: path})} title="Select CDrive Path to Table A"  actionName="Select"
           driveObjects={this.state.driveObjects} type="file" />
           <CDrivePathSelector show={this.state.bPathSelector} toggle={() => this.setState({bPathSelector : false})}
-          action={path => this.setState({bPath: path})} driveObjects={this.state.driveObjects} />
-          <button className="btn btn-secondary btn-lg" onClick={() => this.setState({aPathSelector : true})} >
-            Browse
-          </button>
-          {this.state.aPath}
+          action={path => this.setState({bPath: path})} title="Select CDrive Path to Table B"  actionName="Select"
+          driveObjects={this.state.driveObjects} type="file" />
+          <div className="label-text">
+            Table A:
+          </div>
+          <div className="left-input-container">
+            {aPath}
+            <button className="btn btn-secondary browse-btn" onClick={() => this.setState({aPathSelector : true})} >
+              Browse
+            </button>
+          </div>
+          <div className="right-input-container">
+            <input type="text" placeholder="No of Pieces" value={this.state.nA} className="blocker-text-input"
+              onChange={e => this.setState({nA: e.target.value})} />
+          </div>
+          <div className="label-text">
+            Table B:
+          </div>
+          <div className="left-input-container">
+            {bPath}
+            <button className="btn btn-secondary browse-btn" onClick={() => this.setState({bPathSelector : true})} >
+              Browse
+            </button>
+          </div>
+          <div className="right-input-container">
+            <input type="text" placeholder="No of Pieces" value={this.state.nB} className="blocker-text-input"
+              onChange={e => this.setState({nB: e.target.value})} />
+          </div>
+          <div className="label-text">
+            Block {"Function"} Container:
+          </div>
+          <div className="left-input-container">
+            <input type="text" placeholder="Container URL" value={this.state.containerUrl} className="blocker-text-input"
+              onChange={e => this.setState({containerUrl: e.target.value})} />
+          </div>
+          <div className="right-input-container">
+            <input type="text" placeholder="No of Replicas" value={this.state.replicas} className="blocker-text-input"
+              onChange={e => this.setState({replicas: e.target.value})} />
+          </div>
+          <div className="inputs-container">
+            {blockButton}
+            {abortButton}
+          </div>
         </div>
       );
     }
